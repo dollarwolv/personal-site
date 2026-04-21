7d765d6d0be0ab812a8d515a83aecd67c9429ed5
dollarwolv <dollarwolv@gmail.com>
2026-04-21 14:47:40 -0700

    Updated text and add links

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="mt-12">
-      <div className="mx-auto flex w-fit max-w-160 flex-col items-start gap-4 text-3xl">
+    <div className="mt-12 p-2">
+      <div className="mx-auto flex w-fit max-w-160 flex-col items-start gap-4 text-2xl leading-snug">
         <div className="flex flex-row items-end gap-2">
           <p className="">Justin</p>
           <Image
@@ -15,13 +15,28 @@ export default function Home() {
           />
           <p className="">Dotzlaw</p>
         </div>
-        <p className="">
-          Product + Brand Designer with 15+ years turning complex ideas into
-          products people actually want to use. Across fintech, web3 and SaaS,
-          he has built brands from scratch, shipped products at scale, and
-          guided projects 0→1. Currently consulting at Republic and running
-          Magic x Logic, his independent design practice.
-        </p>
+        <h2 className="">
+          Web developer and AI Master's student with a background in psychology.
+          I’ve co-founded and worked in early-stage startups across growth,
+          product, and engineering, shipping projects end to end.{" "}
+          <span className="text-stone-500">
+            Currently building web applications and working with businesses to
+            create software that solves real problems.
+          </span>
+        </h2>
+        <div className="flex flex-row gap-2 text-sm">
+          <a href="https://www.linkedin.com/in/justindotzlaw/">
+            <button className="cursor-pointer rounded-xl bg-stone-100 px-4 py-2 text-black transition-colors duration-300 hover:bg-black hover:text-white">
+              LinkedIn
+            </button>
+          </a>
+
+          <a href="mailto:justindotzlaw@gmail.com">
+            <button className="cursor-pointer rounded-xl bg-stone-100 px-4 py-2 text-black transition-colors duration-300 hover:bg-black hover:text-white">
+              Mail
+            </button>
+          </a>
+        </div>
       </div>
     </div>
   );
